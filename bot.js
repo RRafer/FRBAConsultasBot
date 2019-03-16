be1585ec7c1d678ba05cbcadeb201364f41c8082
RRafer <federicoragazzo1999@hotmail.com>
2019-03-16 17:18:59 -0300

    TelegramBot v0.2

---
+++ bot.js
@@ -0,0 +1,26 @@
+const TelegramBot = require('node-telegram-bot-api');
+//TOKEN Bot
+const token = '883966977:AAEKL2FShenvXov-h33BKDqikS8_BD0ft-Q';
+
+const bot = new TelegramBot(token, {polling: true});
+
+//Cuando aparece solo esa palabra
+bot.onText(/^\/empieza/, (msg) => {
+    bot.sendMessage(msg.chat.id, "Las materias de 2do a 6to año empiezan el 18\nFisica 1 curso Z empieza el 25\nRecursantes empiezan el 25 de marzo\nIngresantes empiezan el 1 de Abril\n");
+});
+
+// Responde cuando aparece una palabra en un mensaje
+/*bot.on('message', (msg) => { 
+	var cmd1 = "/palabra1";
+	var cmd2 = "/palabra2";
+	var cmd3 = "/palabra3";
+ 	if (msg.text.includes(cmd1)) {
+    	bot.sendMessage(msg.chat.id, "Las materias de 2do a 6to año empiezan el 18\nFisica 1 curso Z empieza el 25\nRecursantes empiezan el 25 de marzo\nIngresantes empiezan el 1 de Abril\n");
+ 	}
+ 	if (msg.text.includes(cmd2)) {
+    	bot.sendMessage(msg.chat.id, "");
+ 	}
+ 	if (msg.text.includes(cmd3)) {
+    	bot.sendMessage(msg.chat.id, "");
+ 	}
+}); */
