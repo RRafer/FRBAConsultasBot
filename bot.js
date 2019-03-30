361c3dbeae472c63e1c37354072b6cad95b9a243
RRafer <federicoragazzo1999@hotmail.com>
2019-03-29 21:04:32 -0300

    Mas funcion bienvenida

--- bot.js
+++ bot.js
@@ -54,6 +54,61 @@ bot.onText(/^\/stadistics/, (msg) => {
     });
 });
 
+bot.on('message', function(msg){
+ 
+    var verify = {
+            "inline_keyboard": [
+                [
+                    {text: "Haga clic aquí", callback_data: 'verificarbot'}
+                ]
+            ]
+        };
+ 
+    var data = {
+        "reply_markup": JSON.stringify(verify)
+    };
+ 
+    /*const perms2 = {};
+    perms.can_send_message = false;
+    perms.can_send_media_messages = false;
+    perms.can_send_other_messages = false;
+    perms.can_add_web_page_previews = false;*/
+ 
+  /*  msg.new_chat_members.forEach(function(){
+ 
+    });*/
+
+    const msgBienvenida = msg.message_id + 1;
+ 	console.log(msgBienvenida)
+
+    if (msg.new_chat_members != undefined){
+        //bot.restrictChatMember(msg.chat.id, msg.new_chat_members.id, perms2).then(function(result){
+            bot.sendMessage(msg.chat.id, "Hola " + msg.new_chat_members[0].first_name + ", bienvenido al grupo de consultas " + msg.chat.title + " de la UTN - FRBA\n\nHaga clic en el boton de abajo para verificar que no sea un bot.", data);
+        //})
+    }
+
+    bot.on('callback_query', function onCallbackQuery(accionboton){
+	    const data = accionboton.data
+	    const msg = accionboton.message
+	   	console.log(accionboton)
+	  /*  const perms = {};
+	    perms.can_send_message = true;
+	    perms.can_send_media_messages = true;
+	    perms.can_send_other_messages = true;
+	    perms.can_add_web_page_previews = true;*/
+	 
+	    if (data == "verificarbot"){
+	        //bot.restrictChatMember(msg.chat.id, msg.new_chat_members.id, perms).then(function(result){
+	            bot.editMessageText(msg.chat.id, msgBienvenida, msg.new_chat_members[0].first_name + " ¡Has sido verificado \u2705!\n\nEste mensaje se borrara en unos minutos")//.then(function (data){
+	/*			    setTimeout(function(){
+				        bot.deleteMessage(data.chat.id, data.message_id);
+				    }, 60000);
+				}*/
+	   //   })
+	    };
+	})	
+});
+ 
 
 
 // Responde cuando aparece una palabra en un mensaje
