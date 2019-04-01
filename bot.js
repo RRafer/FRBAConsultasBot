d258dcaa5ce9d21239a2cf0f6bfb9c7b01807c69
RRafer <federicoragazzo1999@hotmail.com>
2019-04-01 15:56:36 -0300

    /links funcionando - Mejorar code inline keyboard

--- bot.js
+++ bot.js
@@ -2,14 +2,15 @@ const TelegramBot = require('node-telegram-bot-api');
 const mongo = require('mongodb').MongoClient;
 var url = 'mongodb://localhost:27017';
 
-
 //TOKEN Bot
 const token = '883966977:AAEKL2FShenvXov-h33BKDqikS8_BD0ft-Q';
 
 const bot = new TelegramBot(token, {polling: true});
 
+//Muestra errores en consola
 bot.on("polling_error", (msg) => console.log(msg));
 
+// Elimina mensajes de personas que se unen y abandonan el grupo
 bot.on('message', (msg) => {
     if(msg.new_chat_member !== undefined || msg.left_chat_member !== undefined){
         setTimeout(() => { bot.deleteMessage(msg.chat.id, msg.message_id); }, 1500);
@@ -17,9 +18,9 @@ bot.on('message', (msg) => {
 });
 
 
-//Cuando aparece solo esa palabra
+//Envia mensaje con el comando /empieza (Solo cuando esta eso en un mensaje)
 bot.onText(/^\/empieza/, (msg) => {
-  //console.log(msg);
+	//console.log(msg);
     bot.sendMessage(msg.chat.id, "Las materias de 2do a 6to año empiezan el 18\nFisica 1 curso Z empieza el 25\nRecursantes empiezan el 25 de marzo\nIngresantes empiezan el 1 de Abril\n",{reply_to_message_id: msg.message_id});
 });
 
@@ -61,18 +62,296 @@ bot.onText(/[\s\S]+/g,(msg) => {
 */
 
 
-
+//Envia links de grupos y otros
 bot.onText(/^\/links/, (msg) => {
-    let links = require('./resources/links.json')
+    var links = {
+        "inline_keyboard": [
+            [
+                {
+                    text: "Encuesta Docente",
+                    url: "https://docs.google.com/spreadsheets/d/19XPRP8zsVut-W1HihBxSZ_mZHlyHMBy-WIKNqurdNs8/edit?usp=sharing"
+                },
+                {
+                    text: "Calendario académico",
+                    url: "http://siga.frba.utn.edu.ar/up/docs/CalendarioAcademico2019.jpg"
+                }
+            ],
+            [
+                {
+                    text: "UTNianos - Relax (Offtopic)",
+                    url: "https://t.me/joinchat/I56Nlks-UO1tfq-oPo1X8g"
+                },
+                {
+                    text: "Novedades UTN",
+                    url: "https://t.me/joinchat/AAAAAEYtOAZkHDcubu0kTw"
+                }
+            ],
+            [
+                {
+                    text: "Ing. Sistemas",
+                    callback_data: 1
+                },
+                {
+                    text: "Ing. Electrónica",
+                    callback_data: 2
+                }
+            ],
+            [
+                {
+                    text: "Ing. Industrial",
+                    callback_data: 5
+                },
+                {
+                    text: "Ciencias básicas.",
+                    callback_data: 3
+                }
+            ],
+            [
+                {
+                    text: "Ingreso",
+                    callback_data: 4
+                }
+            ]
+        ]
+    };
+
+    var sistemas = {
+        "inline_keyboard": [
+            [
+                {
+                    text: "Consultas",
+                    url: "https://t.me/joinchat/I56NlkYz5zSQlGK5pY2z2Q"
+                }
+            ],
+            [
+                {
+                    text: "Sistemas y Organizaciones",
+                    url: "https://t.me/joinchat/I56NllM1zvwWCJm2kjbDJA"
+                },
+                {
+                    text: "Matemática Discreta",
+                    url: "https://t.me/joinchat/I56Nlk5RT8WzHvJNxS6SZw"
+                }
+            ],
+            [
+                {
+                    text: "Algoritmos y Estructura de Datos",
+                    url: "https://t.me/joinchat/I56NlksXoCPK6v2gFxRCXA"
+                },
+                {
+                    text: "Arquitectura de Computadoras",
+                    url: "https://t.me/joinchat/I56NllGs6dWlkvYmkMnLIw"
+                }
+            ],
+            [
+                {
+                    text: "Analisis de Sistemas",
+                    url: "https://t.me/joinchat/I56NllM1zvwWCJm2kjbDJA"
+                },
+                {
+                    text: "Sintaxis y Semantica de los Lenguajes",
+                    url: "https://t.me/joinchat/I56NlhXhclb8iG52KFBpKg"
+                }
+            ],
+            [
+                {
+                    text: "Paradigmas de Programación",
+                    url: "https://t.me/joinchat/I56Nlkyv4xMgJbAJVnwkRw"
+                }
+            ],
+            [
+                {
+                    text: "Diseño de Sistemas",
+                    url: "https://t.me/joinchat/I56NlhNqcghyL5dP7GpwOA"
+                },
+                {
+                    text: "Gestión de Datos",
+                    url: "https://t.me/joinchat/I56NlliHAUjG7w1Rxm7vMQ"
+                }
+            ],
+            [
+                {
+                    text: "Sistemas Operativos",
+                    url: "https://t.me/joinchat/I56NlkfCkX-G5wb13H9bVA"
+                },
+                {
+                    text: "Matemática Superior",
+                    url: "https://t.me/joinchat/I56NllMgZkgyFQ7pF2AiWA"
+                }
+            ],
+            [
+                {
+                    text: "Técnicas Avanzadas de Programación",
+                    url: "https://t.me/TADPUTN"
+                }
+            ]
+        ]
+    };
+
+    var industrial = {
+        "inline_keyboard": [
+            [
+                {
+                    text: "Consultas",
+                    url: "https://t.me/IndustrialUTN"
+                }
+            ]
+        ]
+    };
+
+    var basica = {
+        "inline_keyboard": [
+            [
+                {
+                    text: "Análisis Matemático I",
+                    url: "https://t.me/AnalisisMatematicoI"
+                },
+                {
+                    text: "Algebra y Geometria Analítica",
+                    url: "https://t.me/algebrautnfrba"
+                }
+            ],
+            [
+                {
+                    text: "Fisica I",
+                    url: "https://t.me/FisicaI"
+                },
+                {
+                    text: "Quimica General",
+                    url: "https://t.me/joinchat/I56NllKkSXk4klHXY9I2lw"
+                }
+            ],
+            [
+                {
+                    text: "Análisis Matemático II",
+                    url: "https://t.me/AnalisisMatematicoII"
+                },
+                {
+                    text: "Probabilidad y Estadistica",
+                    url: "https://t.me/probautn"
+                }
+            ],
+            [
+                {
+                    text: "Fisica II",
+                    url: "https://t.me/joinchat/I56NlkTlE4mRDXeHpmY9XA"
+                }
+            ]
+        ]
+    };
+
+    var electronica = {
+        "inline_keyboard": [
+            [
+                {
+                    text: "Consultas",
+                    url: "https://t.me/joinchat/I56NllK4UcKmSI77brSJvQ"
+                }
+            ],
+            [
+                {
+                    text: "Informatica I",
+                    url: "https://t.me/joinchat/I56NlkXmGlX23GKn7fiZNA"
+                }
+            ]
+        ]
+    };
+
+    var ingreso = {
+        "inline_keyboard": [
+            [
+                {
+                    text: "Módulo B",
+                    url: "https://t.me/ingresoutnfrba"
+                }
+            ],
+            [
+                {
+                    text: "Módulo A",
+                    url: "https://t.me/joinchat/I56Nlk-4xT1jtWiR1_hyCA"
+                }
+            ]
+        ]
+    };
+
     bot.sendMessage(msg.chat.id,"LINKS", {
-        reply_markup: {
-            inline_keyboard: links
-        }
+        "reply_markup": JSON.stringify(links),
+        reply_to_message_id: msg.message_id
     });
+
+    var msgLinks = msg.message_id + 1;
+
+    bot.on('callback_query', function onCallbackQuery(link){
+        console.log(msgLinks);
+        if(link.from.id === link.message.reply_to_message.from.id){
+            switch(link.data){
+                //Sistemas
+                case '1':
+                    var messages = "Links de Ing. Sistemas";
+                    bot.editMessageText(messages, {
+                        chat_id: msg.chat.id,
+                        message_id: msgLinks,
+                        "reply_markup": JSON.stringify(sistemas)
+                    });
+                    break;
+                //Electronica
+                case '2':
+                    var messageel = 'Links de Ing. Electrónica: ';
+
+                    bot.editMessageText(messageel, {
+                        chat_id: msg.chat.id,
+                        message_id: msgLinks,
+                        "reply_markup": JSON.stringify(electronica)
+                    });
+                    break;
+                //Basicas
+                case '3':
+                    var messageb = 'Links de Ciencias Basicas: ';
+
+                    bot.editMessageText(messageb, {
+                        chat_id: msg.chat.id,
+                        message_id: msglinks,
+                        "reply_markup": JSON.stringify(basica)
+                    });
+                    break;
+                //Ingreso
+                case '4':
+                    var messageing = 'Links de Ingreso: ';
+
+                    bot.editMessageText(messageing, {
+                        chat_id: msg.chat.id,
+                        message_id: msgLinks,
+                        "reply_markup": JSON.stringify(ingreso)
+                    });
+                    break;
+                //Industrial
+                case '5':
+                    var messageind = 'Links de Ing. Industrial: ';
+
+                    bot.editMessageText(messageind, {
+                        chat_id: msg.chat.id,
+                        message_id: msgLinks,
+                        "reply_markup": JSON.stringify(industrial)
+                    });
+                    break;
+                default:
+                    break;
+            }
+        }
+        else{
+            bot.answerCallbackQuery({
+                callback_query_id: link.id,
+                text: "No puede verificar por otro usuario",
+                show_alert: true
+            });
+        }
+    })
 });
 
 
 
+
+//Verificación usuarios
 bot.on('message', function(msg){
     var verify = {
             "inline_keyboard": [
