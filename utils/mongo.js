5066491a955ddc10f684838bc4136c282e64ef7f
Muabid <bruno2698@gmail.com>
2019-04-01 22:31:09 -0300

    Ordeno el proyecto

--- utils/mongo.js
+++ utils/mongo.js
@@ -1,29 +1,18 @@
-const users = require('./mongo/users');
-const errors = require('./mongo/errors');
+const mongo = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017';
 
-exports.insertChatId = (userId, chatId) => users.insertChatId(userId, chatId).catch(err => errors.logError(err, chatId));
-exports.getGroupIds = (userId, chatId) => users.getGroupIds(userId).catch(err => errors.logError(err, chatId));
-exports.logError = (error, chatId) => errors.logError(error, chatId);
 
-// Mongo Singleton Use Example
-// Placeholder name, change for each collection.
-const collectionName = 'mensajes';
+exports.insertMessage =(msg) =>{
+    mongo.connect(url, (err, client) => {
 
-async function getdocs() {
-	let collection;
-	(await db.getDb()).then((db)=> {collection = db.collection(collectionName);});
-	collection.find({}).toArray(function(err, docs) {
-		if (!err)
-			console.log(`Se han cargado ${docs.length} documentos`);
-	});
+
+        const db = client.db('BotTelegram');
+
+        db.collection("messages").insertOne(msg, (err, res) => {
+            if (err) throw err;
+
+            client.close();
+
+        });
+    });
 }
-
-getdocs();
-
-//#region Comentarios
-
-// exports.setStartedUser = (userId, chatId) => users.setStartedUser(userId).catch(err => errors.logError(err, chatId));
-// exports.getPrivateChatId = (userId, chatId) => users.getPrivateChatId(userId).catch(err => errors.logError(err, chatId));
-// exports.hasStarted = (userId, chatId) => users.hasStarted(userId).catch(err => errors.logError(err, chatId));
-
-//#endregion
