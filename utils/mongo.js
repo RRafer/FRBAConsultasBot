701c8ca98c7c1f120d68d03526c4ca429a14aa11
nico2236 <nico2236@gmail.com>
2019-05-16 19:11:31 -0300

    Primera Version del Branch

--- utils/mongo.js
+++ utils/mongo.js
@@ -1,18 +1,16 @@
 const mongo = require('mongodb').MongoClient;
+
 const url = 'mongodb://localhost:27017';
 
 
-exports.insertMessage =(msg) =>{
-    mongo.connect(url, (err, client) => {
+exports.insertMessage = (msg) => {
+  mongo.connect(url, (err, client) => {
+    const db = client.db('BotTelegram');
+    if (err) throw err;
+    db.collection('messages').insertOne(msg, (error, result) => {
+      if (error) throw err;
 
-
-        const db = client.db('BotTelegram');
-
-        db.collection("messages").insertOne(msg, (err, res) => {
-            if (err) throw err;
-
-            client.close();
-
-        });
+      client.close();
     });
-}
+  });
+};
