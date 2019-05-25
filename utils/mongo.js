bae73ef203a7060b7e78c77eb6b3c66b3cb5c3cc
Kvothe838 <fyuffrida@baufest.com>
2019-05-24 22:20:41 -0300

    Prueba.

--- utils/mongo.js
+++ utils/mongo.js
@@ -1,16 +1,40 @@
 const mongo = require('mongodb').MongoClient;
-
 const url = 'mongodb://localhost:27017';
-
 
 exports.insertMessage = (msg) => {
   mongo.connect(url, (err, client) => {
-    const db = client.db('BotTelegram');
-    if (err) throw err;
+    const db = client.db('telegram');
+    if(err) throw err;
     db.collection('messages').insertOne(msg, (error, result) => {
-      if (error) throw err;
+      if(error) throw err;
 
       client.close();
+    });
+  });
+};
+
+exports.insertUser = (userId, privateChatId) => {
+  monog.connect(url, (err, client) => {
+    const db = client.db('telegram');
+    if(err) throw err;
+    db.collection('user').insertOne({"userId": userId, "privateChatId": privateChatId}, (error, result) => {
+      if(error) throw err;
+
+      client.close();
+    });
+  });
+}
+
+exports.getPrivateChatId = userId => {
+  mongo.connect(url, (err, client) => {
+    const db = client.db('telegram');
+    if(err) throw err;
+    db.collection('user').find({"userId": userId}, (error, result) => {
+      if(error) throw err;
+      
+      client.close();
+
+      return result.privateChat;
     });
   });
 };
