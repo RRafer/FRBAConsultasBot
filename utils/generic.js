53b68e85b7110ffed46e4ce700d81bee352d24a5
Kvothe838 <fyuffrida@baufest.com>
2019-05-25 18:27:22 -0300

    Emprolijado de código en subdivisión de archivos y archivo con constantes y funciones genéricas.

--- utils/generic.js
+++ utils/generic.js
@@ -1,64 +1,14 @@
-exports.generateMention = (msg) => {
-	const {username, first_name, last_name, id} = msg.from;
-	const fromName = `${first_name}${last_name ? ` ${last_name}` : ''}`;
-	return `[@${username||fromName}](tg://user?id=${id})`;
-};
-
-exports.generateTimeInMiliseconds = (typeOfUnit, amount) => {
-	var timeToSet = 0;
-	var spanishTypeOfUnit = '';
-	var isOneUnit = amount == 1;
-	const days = 'd';
-	const hours = 'h';
-	const minutes = 'm';
-	const seconds = 's';
-	const weeks = 'w';
-
-	switch(typeOfUnit)
-	{
-	case days:
-		timeToSet = amount * 24 * 60 * 60;
-		spanishTypeOfUnit = isOneUnit ? 'día' : 'días';
-		break;
-	case hours:
-		timeToSet = amount * 60 * 60;            
-		spanishTypeOfUnit = isOneUnit ? 'hora' : 'horas';      
-		break;
-	case minutes:
-		timeToSet = amount * 60;
-		spanishTypeOfUnit = isOneUnit ? 'minuto' : 'minutos';
-		break;
-	case seconds:
-		timeToSet = amount;
-		spanishTypeOfUnit = isOneUnit ? 'segundo' : 'segundos';
-		break;
-	case weeks:
-		timeToSet = amount * 7 * 24 * 60 * 60;
-		spanishTypeOfUnit = isOneUnit ? 'semana' : 'semanas';
-		break;
-	}
-
-	return [timeToSet, spanishTypeOfUnit];
-};
-
-exports.logArray = array => {
-	console.log(JSON.stringify(array), null, '\n');
-};
-
-/**
- * Obtiene la n posición del substring en el string, donde n es index.
- * @param { string } string El string en el que se va a buscar la posición.
- * @param { string } substring El substring que se va a buscar.
- * @param { number } index La posición que se va a buscar del substring en el string comenzando desde 1.
- */
-exports.getPosition = (string, subString, index) => {
-	return string.split(subString, index).join(subString).length;
-};
-
+exports.generateMention = msg => {
+    var fromUsername = msg.from.username;
+    var fromName = msg.from.first_name + (msg.from.last_name != undefined ? ' ' + msg.from.last_name : '');
+    var userId = msg.from.id;
+    
+    return '[' + (fromUsername != undefined ? '@' + fromUsername : fromName) + '](tg://user?id=' + userId + ')'
+}
 exports.space = ' ';
 exports.lineBreak = '\n';
 exports.markDownParseMode = 'Markdown';
-exports.statusKicked = 'kicked';
+exports.statusKick = 'kick';
 exports.statusLeft = 'left';
 exports.statusCreator = 'creator';
 exports.statusAdministrator = 'administrator';
--- utils/onText/banKick.js
+++ utils/onText/banKick.js
@@ -1,155 +1,70 @@
 const generic = require('../generic');
 
 exports.execute = (bot, msg, match) => {
-    return new Promise((resolve, reject) => {
-        if(msg.reply_to_message == undefined || msg.text == '/banall')
+    if(msg.reply_to_message == undefined)
+    {
+        return;
+    }
+
+    var chatId = msg.chat.id;
+    var userId = msg.from.id;
+    var replyId = msg.reply_to_message.from.id;    
+    var replyUsername = msg.reply_to_message.from.username;
+    var banOrKick = match[1];
+    var numberOfSeconds = match[2];
+    numberOfSeconds.replace(' ', '');
+
+    switch(replyUsername)
+    {
+        case generic.botUserName:
+            return;        
+    }    
+
+    bot.getChatMember(chatId, replyId).then(replyMember => {
+        switch(replyMember.status)
         {
-            resolve();
-            return;
+            case generic.statusKick:
+            case generic.statusLeft:
+            case generic.statusCreator:
+            case generic.statusAdministrator:
+                return;
         }
 
-        const chatId = msg.chat.id;
-        const userId = msg.from.id;
-        const replyId = msg.reply_to_message.from.id;    
-        const replyUsername = msg.reply_to_message.from.username;
-        const banOrKick = match[1];
-        const maybeParams = match[2];
-        const areParams = maybeParams != undefined && maybeParams.replace(' ', '') != '';
-        var params = [];
-        var amount = 0;
-        var typeOfUnit = '';
-        var timeToSet = 0;
-        var spanishTypeOfUnit = '';
-        
-        if(areParams)
-        {
-            try
+        bot.getChatMember(chatId, userId).then(userMember => {
+            const userMention = generic.generateMention(msg);
+            switch(userMember.status)
             {
-                params = maybeParams.split(' ');
-                const maybeAmount = params[1];
-
-                if(params.length != 3 || isNaN(maybeAmount)) 
-                {
-                    resolve();
-                    return;
-                }
-                
-                amount = parseInt(maybeAmount);
-                typeOfUnit = params[2];
-                result = generic.generateTimeInMiliseconds(typeOfUnit, amount);
-                timeToSet = result[0];
-                spanishTypeOfUnit = result[1];
-            }
-            catch(error)
-            {
-                reject(error);
-                return;
-            }            
-        }       
-
-        switch(replyUsername)
-        {
-            case generic.botUserName:
-                resolve();
-                return;
-        }    
-
-        bot.getChatMember(chatId, replyId).then(replyMember => {
-            switch(replyMember.status)
-            {
-                case generic.statusKick:
-                case generic.statusLeft:
                 case generic.statusCreator:
                 case generic.statusAdministrator:
-                    resolve();
-                    return;
-            }
-
-            bot.getChatMember(chatId, userId).then(userMember => {
-                const userMention = generic.generateMention(msg);
-                switch(userMember.status)
-                {
-                    case generic.statusCreator:
-                    case generic.statusAdministrator:
-                        const replyMention = generic.generateMention(msg.reply_to_message);
-                        switch (banOrKick)
-                        {
-                            case 'ban':                       
-                                if(areParams)
-                                {
-                                    bot.kickChatMember(chatId, replyId, {until_date: Math.round((Date.now() + (timeToSet * 1000)) / 1000)}).then(() => {
-                                        bot.sendMessage(chatId, userMention + ' ha baneado a ' + replyMention + "  durante " + amount + ' ' + spanishTypeOfUnit + '!', { parse_mode: generic.markDownParseMode })
-                                            .then(() => {
-                                                resolve();
-                                                return;
-                                            })
-                                            .catch(e => {
-                                                reject(e);
-                                                return;
-                                            });
-                                    }).catch(e => {
-                                        reject(e);
-                                        return;
-                                    });
-                                }
-                                else
-                                {
-                                    bot.kickChatMember(chatId, replyId).then(() => {
-                                        bot.sendMessage(chatId, userMention + ' ha baneado a ' + replyMention + ' indefinidamente!', { parse_mode: generic.markDownParseMode })
-                                            .then(() => resolve())
-                                            .catch(e => {
-                                                reject(e);
-                                                return;
-                                            });
-                                    }).catch(e => {
-                                        reject(e);
-                                        return;
-                                    });
-                                }
-                                break;
-                            case 'kick':
+                    const replyMention = generic.generateMention(msg.reply_to_message);
+                    switch (banOrKick)
+                    {
+                        case 'ban':                            
+                            if(numberOfSeconds == '' || isNaN(numberOfSeconds))
+                            {
                                 bot.kickChatMember(chatId, replyId).then(() => {
-                                    bot.unbanChatMember(chatId, replyId).then(() => {
-                                        bot.sendMessage(chatId, userMention + ' ha kickeado a ' + replyMention + '!', { parse_mode: generic.markDownParseMode })
-                                            .then(() => {
-                                                resolve();
-                                                return;
-                                            })
-                                            .catch(e => {
-                                                reject(e);
-                                                return;
-                                            });
-                                    }).catch(e => {
-                                        reject(e);
-                                        return;
-                                    });                               
-                                }).catch(e => {
-                                    reject(e);
-                                    return;
+                                    bot.sendMessage(chatId, userMention + ' ha baneado a ' + replyMention + ' indefinidamente!', { parse_mode: generic.markDownParseMode });
                                 });
-                                break;
-                        }
-                        break;
-                    default:
-                        bot.sendMessage(chatId, "Sory" + userMention + " pero no sos admin.", { parse_mode: generic.markDownParseMode })
-                            .then(() => {
-                                resolve();
-                                return;
-                            })
-                            .catch(e => {
-                                reject(e);
-                                return;
+                            }
+                            else
+                            {
+                                bot.kickChatMember(chatId, replyId, {until_date: Math.round((Date.now() + (parseInt(numberOfSeconds) * 1000)) / 1000)}).then(() => {
+                                    bot.sendMessage(chatId, userMention + ' ha baneado a ' + replyMention + "  durante " + numberOfSeconds + " segundos!", { parse_mode: generic.markDownParseMode });
+                                });
+                            }
+                            break;
+                        case 'kick':
+                            bot.kickChatMember(chatId, replyId).then(() => {
+                                bot.unbanChatMember(chatId, replyId);                  
+                                    bot.sendMessage(chatId, userMention + ' ha kickeado a ' + replyMention + '!', { parse_mode: generic.markDownParseMode });
                             });
-                        break;
-                }
-            }).catch(e => {
-                reject(e);
-                return;
-            });
-        }).catch(e => {
-            reject(e);
-            return;
+                            break;
+                    }
+                    break;
+                default:
+                    bot.sendMessage(chatId, "Sory" + userMention + " pero no sos admin.", { parse_mode: generic.markDownParseMode });
+                    break;
+            }
         });
     });
-    
 };
