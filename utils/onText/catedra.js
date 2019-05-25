0a11f4dc446b6a5f9af21e409c1d7ad9fa405209
Kvothe838 <fyuffrida@baufest.com>
2019-05-25 18:49:48 -0300

    Verdadero merge.

--- utils/onText/catedra.js
+++ utils/onText/catedra.js
@@ -1,19 +1,3 @@
 exports.execute = (bot, msg) => {
-    var chatPos = idChatPhoto.indexOf(msg.chat.id);
-    if(chatPos === -1)
-    {
-        var auxid = bot.sendPhoto(msg.chat.id, 'AgADAQADEagxG_BImEVSfV4Gc0JIbXLqCjAABFlZxFZk81qhMjcDAAEC').then((auxid) => {
-        setTimeout(() => {
-            borrarchat = idChatPhoto.indexOf(auxid.chat.id);
-            idChatPhoto.splice(borrarchat, 1);
-            idPhoto.splice(borrarchat, 1)
-        }, 60000);
-        idChatPhoto.push(auxid.chat.id);
-        idPhoto.push(auxid.message_id)
-        });
-    }
-    else
-    {
-        bot.sendMessage(idChatPhoto[chatPos], "Has solicitado el comando muy pronto. Aqui tienes la ultima vez que el comando ha sido usado", {reply_to_message_id: idPhoto[chatPos]});
-    }
+    
 }
