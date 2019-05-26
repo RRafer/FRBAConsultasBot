2f66e1efd6f240cccf19601c0cc63da34852e53f
Kvothe838 <fyuffrida@baufest.com>
2019-05-25 22:00:31 -0300

    Se agrega a la BD para /start y /prueba que más adelante será newmember

--- utils/mongo/users.js
+++ utils/mongo/users.js
@@ -1,103 +1,170 @@
-// Refactor this in another branch
 const mongo = require('mongodb').MongoClient;
 const url = 'mongodb://localhost:27017';
 
 //Estructura:
 //userId - number
 //chatIds - array de number
+//privateChatId - number
+
+exports.insertPrivateChat = (userId, privateChatId) => {
+  return new Promise((resolve, reject) => {
+    mongo.connect(url, (err, client) => {
+      const db = client.db('telegram');
+      const col = db.collection('users');
+
+      if(err) reject();
+      col.find({'userId': userId}).count((error, result) => {
+        if(error) reject();
+
+        if(result > 0)
+        {
+          col.findOneAndUpdate({'userId': userId}, {$set: {'privateChatId': privateChatId}}, (error, result) => {            
+            client.close()
+
+            if(error) reject();
+
+            resolve()
+          });
+        }
+        else
+        {
+          col.insertOne({'userId': userId, 'chatIds': [], 'privateChatId': privateChatId}, (error, result) => {            
+            client.close();
+
+            if(error) reject();
+
+            resolve();
+          });
+        }        
+      });    
+    });
+  });
+}
 
 exports.insertChatId = (userId, chatId) => {
-	return new Promise((resolve, reject) => {
-		mongo.connect(url, (err, client) => {
-			const db = client.db('telegram');
-			const col = db.collection('users');
+  return new Promise((resolve, reject) => {
+    mongo.connect(url, (err, client) => {
+      const db = client.db('telegram');
+      const col = db.collection('users');
 
-			if(err) reject(new Error('Error de conexión con Mongo.'));
+      if(err) reject();
       
-			col.findOne({'userId': userId}, (error, result) => {
-				if(error)
-				{
-					reject(error);
-					return;
-				}
+      col.findOne({'userId': userId}, (error, result) => {
+        if(error) reject();
 
-				if(result != null)
-				{
-					if(result.chatIds.includes(chatId))
-					{
-						resolve();
-						return;
-					}
+        if(result != null)
+        {
+          if(result.chatIds.includes(chatId)) reject()
           
-					col.findOneAndUpdate({'userId': userId}, {$push: {'chatIds': chatId}}, (findErr, result) => {
-						client.close();
+          col.findOneAndUpdate({'userId': userId}, {$push: {'chatIds': chatId}}, (error, result) => {
+            client.close()
 
-						if(findErr)
-						{
-							reject(findErr);
-							return;
-						}
+            if(error) reject();
 
-						resolve();
-						return;
-					});
-				}
-				else
-				{
-					col.insertOne({'userId': userId, 'chatIds': [chatId]}, (error, result) => {
-						client.close();
+            resolve()
+          });
+        }
+        else
+        {
+          col.insertOne({'userId': userId, 'chatIds': [chatId], 'privateChatId': 0}, (error, result) => {
+            client.close();
 
-						if(error)
-						{
-							reject(error);
-							return;
-						}
+            if(error) reject();
 
-						resolve();
-						return;
-					});
-				}        
-			});    
-		});  
-	});
+            resolve();
+          });
+        }        
+      });    
+    });
+  });
+}
+
+exports.getPrivateChatId = userId => {
+  return new Promise((resolve, reject) => {
+    mongo.connect(url, (err, client) => {
+      const db = client.db('telegram');
+
+      if(err)
+      {
+        reject()
+      }
+
+      db.collection('users').findOne({"userId": userId}, (error, result) => {
+        if(error)
+        {
+          reject();
+        }
+  
+        client.close();
+        
+        if(result == null)
+        {
+          reject();
+        }
+        else
+        {
+          resolve(result.privateChatId);
+        }
+      });
+    });
+  });
+};
+
+exports.hasStarted = userId => {
+  return new Promise((resolve, reject) => {
+    mongo.connect(url, (err, client) => {
+      const db = client.db('telegram');
+
+      if(err)
+      {
+        reject()
+      }
+
+      db.collection('users').findOne({"userId": userId}, (error, result) => {
+        if(error)
+        {
+          reject();
+        }
+  
+        client.close();        
+        resolve(result != undefined && result.privateChatId != 0);
+      });
+    });
+  });
 };
 
 exports.getGroupIds = userId => {
-	return new Promise((resolve, reject) => {
-		mongo.connect(url, (err, client) => {
-			const db = client.db('telegram');
+  return new Promise((resolve, reject) => {
+    mongo.connect(url, (err, client) => {
+      const db = client.db('telegram');
 
-			if(err)
-			{
-				reject(err);
-				return;
-			}
-			db.collection('users').findOne({'userId': userId}, (error, result) => {
-				if(error)
-				{
-					reject(error);
-					return;
-				}  
+      if(err)
+      {
+        reject()
+      }
 
-				if(result == null)
-				{
-					reject(new Error('No hay chatIds registrados para el userId ' + userId));
-				}
-				else
-				{
-					resolve(result.chatIds);
-				}
-
-				client.close();
-
-				return;
-			});
-		});
-	});
-};
+      db.collection('users').findOne({"userId": userId}, (error, result) => {
+        if(error)
+        {
+          reject();
+        }
+  
+        client.close();
+        
+        if(result == null)
+        {
+          reject();
+        }
+        else
+        {
+          resolve(result.chatIds);
+        }
+      });
+    });
+  });
+}
 
 //#region Comentarios
-
 // exports.insertMessage = (msg) => {
 //   mongo.connect(url, (err, client) => {
 //     const db = client.db('telegram');
@@ -109,95 +176,4 @@ exports.getGroupIds = userId => {
 //     });
 //   });
 // };
-
-// exports.hasStarted = userId => {
-//   return new Promise((resolve, reject) => {
-//     mongo.connect(url, (err, client) => {
-//       const db = client.db('telegram');
-
-//       if(err)
-//       {
-//         reject()
-//       }
-
-//       db.collection('users').findOne({"userId": userId}, (error, result) => {
-//         if(error)
-//         {
-//           reject();
-//         }
-  
-//         client.close();        
-//         resolve(result != undefined && result.privateChatId != 0);
-//       });
-//     });
-//   });
-// };
-
-// exports.setStartedUser = userId => {  
-//   return new Promise((resolve, reject) => {
-//     mongo.connect(url, (err, client) => {
-//       const db = client.db('telegram');
-//       const col = db.collection('users');
-  
-//       if(err) reject();
-  
-//       col.find({'userId': userId}).count((error, result) => {
-//         if(error) reject();
-  
-//         if(result == 1)
-//         {
-//           col.findOneAndUpdate({'userId': userId}, {$set: {'hasStarted': true}}, (findError, result) => {            
-//             client.close();
-  
-//             if(findError) reject();
-  
-//             resolve();
-//           });
-//         }
-//         else if(result == 0)
-//         {
-//           col.insertOne({'userId': userId, 'chatIds': [], 'hasStarted': true}, (insertError, result) => {            
-//             client.close();
-  
-//             if(insertError) reject();
-  
-//             resolve();
-//           });
-//         }        
-//       });    
-//     });
-//   });    
-// }
-
-// exports.getPrivateChatId = userId => {
-//   return new Promise((resolve, reject) => {
-//     mongo.connect(url, (err, client) => {
-//       const db = client.db('telegram');
-
-//       if(err)
-//       {
-//         reject()
-//       }
-
-//       db.collection('users').findOne({"userId": userId}, (error, result) => {
-//         if(error)
-//         {
-//           reject();
-//         }
-  
-//         client.close();
-        
-//         if(result == null)
-//         {
-//           reject();
-//         }
-//         else
-//         {
-//           resolve(result.privateChatId);
-//         }
-//       });
-//     });
-//   });
-// };
-
 //#endregion
