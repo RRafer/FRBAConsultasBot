7ce119f2c80a3ce059afa2b2389f6504e8e45b67
Kvothe838 <fyuffrida@baufest.com>
2019-05-25 22:11:46 -0300

    Se implementa logError.

--- utils/mongo/errors.js
+++ utils/mongo/errors.js
@@ -1,29 +1,23 @@
-// @ts-check
-// Refactored this on another branch already
 const mongo = require('mongodb').MongoClient;
-const logger = require('../../controllers/logger');
-
 const url = 'mongodb://localhost:27017';
 
 //Estructura:
 //chatId - number
 //errorMsg - string
-//type - string
 
 exports.logError = (error, chatId) => {
-	logger.error(`Unpecified Error in errors.js: ${error}`);
-	mongo.connect(url, (err, client) => {
-		if(client == undefined || client == null) return;
-        
-		const db = client.db('telegram');
-		const col = db.collection('errors');
+    mongo.connect(url, (err, client) => {
+        const db = client.db('telegram');
+        const col = db.collection('errors');
   
-		if(err || !error) return;
+        if(err) reject();    
         
-		col.insertOne({'chatId': chatId, 'errorMsg': error.message, 'date': new Date().toDateString()}, (err, result) => {
-			client.close();
+        col.insertOne({'chatId': chatId, 'errorMsg': error}, (error, result) => {
+            client.close();
 
-			if(err) return;
-		});
-	});
-};
+            if(error) reject();
+
+            resolve();
+        });
+      });
+}
