571b6cc3d499fa85d83b220a5ff11014e06116a2
Kvothe838 <fyuffrida@baufest.com>
2019-05-25 22:11:46 -0300

    Se implementa logError.

--- utils/mongo/errors.js
+++ utils/mongo/errors.js
@@ -4,30 +4,20 @@ const url = 'mongodb://localhost:27017';
 //Estructura:
 //chatId - number
 //errorMsg - string
-//type - string
 
 exports.logError = (error, chatId) => {
-    console.log("HUBO UN ERROR: " + error);
     mongo.connect(url, (err, client) => {
-        try
-        {
-            const db = client.db('telegram');
-            const col = db.collection('errors');
-            
-            if(err || !error){
-                client.close();
-                return;
-            }
-            
-            col.insertOne({'chatId': chatId, 'errorName': error.name, 'lineNumber': error.lineNumber, 'errorMsg': error.message}, (err, result) => {
-                client.close();
-    
-                if(err) return;
-            }).catch(e => console.log(e));
-        }
-        catch(e)
-        {
-            console.log(e);
-        }        
-    });
+        const db = client.db('telegram');
+        const col = db.collection('errors');
+  
+        if(err) reject();    
+        
+        col.insertOne({'chatId': chatId, 'errorMsg': error}, (error, result) => {
+            client.close();
+
+            if(error) reject();
+
+            resolve();
+        });
+      });
 }
