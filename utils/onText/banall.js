dc2d989c3e35fb9b71c5f30c142377b980b02880
Kvothe838 <fyuffrida@baufest.com>
2019-05-25 22:00:31 -0300

    Se agrega a la BD para /start y /prueba que más adelante será newmember

---
+++ utils/onText/banall.js
@@ -0,0 +1,51 @@
+const generic = require('../generic');
+const mongo = require('../mongo');
+
+exports.execute = (bot, msg) => {
+    if(msg.reply_to_message == undefined)
+    {
+        return;
+    }
+
+    var chatId = msg.chat.id;
+    var userId = msg.from.id;
+    var replyId = msg.reply_to_message.from.id;    
+    var replyUsername = msg.reply_to_message.from.username;
+
+    switch(replyUsername)
+    {
+        case generic.botUserName:
+            return;        
+    }    
+
+    bot.getChatMember(chatId, replyId).then(replyMember => {
+        switch(replyMember.status)
+        {
+            case generic.statusKick:
+            case generic.statusLeft:
+            case generic.statusCreator:
+            case generic.statusAdministrator:
+                return;
+        }
+
+        bot.getChatMember(chatId, userId).then(userMember => {
+            const userMention = generic.generateMention(msg);
+            switch(userMember.status)
+            {
+                case generic.statusCreator:
+                case generic.statusAdministrator:
+                    const replyMention = generic.generateMention(msg.reply_to_message);
+                    mongo.getGroupIds(userId).then(res => {
+                        res.forEach(chatGroupId => {
+                            bot.kickChatMember(chatGroupId, replyId).then(() => {                                               
+                                bot.sendMessage(chatGroupId, userMention + ' ha kickeado a ' + replyMention + '!', { parse_mode: generic.markDownParseMode });
+                            });
+                        });
+                    });                    
+                default:
+                    bot.sendMessage(chatId, "Sory" + userMention + " pero no sos admin.", { parse_mode: generic.markDownParseMode });
+                    break;
+            }
+        });
+    });
+};
