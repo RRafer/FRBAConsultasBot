cf4fdd702657d0b3cff5885f86cedc27b635ff5d
Kvothe838 <fyuffrida@baufest.com>
2019-05-26 12:57:15 -0300

    WIP banall.

--- utils/mongo.js
+++ utils/mongo.js
@@ -1,9 +1,9 @@
 const users = require('./mongo/users');
 const errors = require('./mongo/errors');
 
-exports.insertPrivateChat = (userId, privateChatId) => users.insertPrivateChat(userId, privateChatId);
-exports.insertChatId = (userId, chatId) => users.insertChatId(userId, chatId);
-exports.getPrivateChatId = userId => users.getPrivateChatId(userId);
-exports.hasStarted = userId => users.hasStarted(userId);
-exports.getGroupIds = userId => users.getGroupIds(userId);
-exports.logError = (userId, chatId) => errors.logError(userId, chatId);
+exports.insertPrivateChat = (userId, privateChatId) => users.insertPrivateChat(userId, privateChatId).catch(err => errors.logError(err, privateChatId));
+exports.insertChatId = (userId, chatId) => users.insertChatId(userId, chatId).catch(err => errors.logError(err, chatId));
+exports.getPrivateChatId = (userId, chatId) => users.getPrivateChatId(userId).catch(err => errors.logError(err, chatId));
+exports.hasStarted = (userId, chatId) => users.hasStarted(userId).catch(err => errors.logError(err, chatId));
+exports.getGroupIds = (userId, chatId) => users.getGroupIds(userId).catch(err => errors.logError(err, chatId));
+exports.logError = (error, chatId) => errors.logError(error, chatId);
--- utils/onText/onText.js
+++ utils/onText/onText.js
@@ -3,23 +3,63 @@ const help = require('./help');
 const banKick = require('./banKick');
 const remindme = require('./remindme');
 const start = require('./start');
+const banall = require('./banall');
 const catedra = require('./catedra');
 const stickers = require('./stickers');
 
-exports.start = (bot, msg) => {   
-  start.execute(bot, msg).catch(err => {
+exports.start = (bot, msg) => {
+  try
+  {  
+    start.execute(bot, msg);
+  }
+  catch(err)
+  {
     const messageError = 'Hubo un error. Por favor, mandame /start en unos minutos.';
+      
     bot.sendMessage(msg.chat.id, messageError)
       .catch(err => mongo.logError(err, chatId));
     mongo.logError(err, msg.chat.id);
-  });  
+  }
 }
-
-exports.remindme = (bot, msg, match) => remindme.execute(bot, msg, match).catch(err => mongo.logError(err, msg.chat.id));
-
-exports.banKick = (bot, msg, match) => banKick.execute(bot, msg, match).catch(err => mongo.logError(err, msg.chat.id));
-
-exports.help = (bot, msg) => help.execute(bot, msg, mongo).catch(err => mongo.logError(err, msg.chat.id));
-
+exports.banall = (bot, msg) => {
+  try
+  {
+    banall.execute(bot, msg);
+  }
+  catch(err)
+  {
+    mongo.logError(err, msg.chat.id);
+  }
+}
+exports.remindme = (bot, msg, match) => {
+  try
+  {
+    remindme.execute(bot, msg, match);
+  }
+  catch(err)
+  {
+    mongo.logError(err, msg.chat.id)
+  }
+}
+exports.banKick = (bot, msg, match) => {
+  try
+  {
+    banKick.execute(bot, msg, match);
+  }
+  catch(err)
+  {
+    mongo.logError(err, msg.chat.id);
+  }
+}
+exports.help = (bot, msg) => {
+  try
+  {
+    help.execute(bot, msg, mongo);
+  }
+  catch(err)
+  {
+    mongo.logError(err, msg.chat.id);
+  }
+};
 //exports.catedra
 //exports.stickers
