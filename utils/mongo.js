b517230fb737cbb99db405bd24df60b69f016225
Facundo Yuffrida <fyuffrida@baufest.com>
2019-06-07 14:04:54 -0300

    Cambio de entidad users en la BD.

--- utils/mongo.js
+++ utils/mongo.js
@@ -1,7 +1,7 @@
 const users = require('./mongo/users');
 const errors = require('./mongo/errors');
 
-exports.insertPrivateChat = (userId, privateChatId) => users.insertPrivateChat(userId, privateChatId).catch(err => errors.logError(err, privateChatId));
+exports.setStartedUser = (userId, chatId) => users.setStartedUser(userId).catch(err => errors.logError(err, chatId));
 exports.insertChatId = (userId, chatId) => users.insertChatId(userId, chatId).catch(err => errors.logError(err, chatId));
 exports.getPrivateChatId = (userId, chatId) => users.getPrivateChatId(userId).catch(err => errors.logError(err, chatId));
 exports.hasStarted = (userId, chatId) => users.hasStarted(userId).catch(err => errors.logError(err, chatId));
