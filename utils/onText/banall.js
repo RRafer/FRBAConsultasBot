c0ac4b46cf48a52e8b08565fcbda3a41c33b5322
Facundo Yuffrida <fyuffrida@baufest.com>
2019-06-10 00:27:40 -0300

    Refacciones de try and catch por Promises, mejora de /ban y fix de /remindme.

--- utils/onText/banall.js
+++ utils/onText/banall.js
@@ -3,70 +3,70 @@ const mongo = require('../mongo');
 
 
 exports.execute = (bot, msg) => {
-    if(msg.reply_to_message == undefined)
-    {
-        return;
-    }
+    return new Promise((resolve, reject) => {
+        if(msg.reply_to_message == undefined)
+        {
+            resolve();
+        }
 
-    var chatId = msg.chat.id;
-    var userId = msg.from.id;
-    var replyId = msg.reply_to_message.from.id;    
-    var replyUsername = msg.reply_to_message.from.username;
+        var chatId = msg.chat.id;
+        var userId = msg.from.id;
+        var replyId = msg.reply_to_message.from.id;    
+        var replyUsername = msg.reply_to_message.from.username;
 
-    switch(replyUsername)
-    {
-        case generic.botUserName:
-            return;        
-    }
+        switch(replyUsername)
+        {
+            case generic.botUserName:
+                resolve();
+        }
 
-    const replyMention = generic.generateMention(msg.reply_to_message);
-    const userMention = generic.generateMention(msg);
+        const replyMention = generic.generateMention(msg.reply_to_message);
+        const userMention = generic.generateMention(msg);
 
-    mongo.getGroupIds(userId, chatId).then(res => {
-        res.forEach(chatGroupId => {
-            bot.getChatMember(chatGroupId, replyId).then(member => {
-                switch(member.status)
-                {
-                    case generic.statusKick:
-                    case generic.statusLeft:
-                    case generic.statusCreator:
-                    case generic.statusAdministrator:
-                        return; //MODIFICAR: No debe hacer return, debe salir 
-                }
-
-                bot.getChatMember(chatId, userId).then(userMember => {                    
-        
-                    switch(userMember.status)
+        mongo.getGroupIds(userId, chatId).then(res => {
+            res.forEach(chatGroupId => {
+                bot.getChatMember(chatGroupId, replyId).then(member => {
+                    switch(member.status)
                     {
+                        case generic.statusKick:
+                        case generic.statusLeft:
                         case generic.statusCreator:
                         case generic.statusAdministrator:
-                            bot.kickChatMember(chatGroupId, replyId).then(() => {
-                                bot.sendMessage(chatGroupId, userMention + ' ha baneado a ' + replyMention + '!', { parse_mode: generic.markDownParseMode })
-                                    .catch(err => mongo.logError(err, chatGroupId));;
-                            })
-                            .catch(err => {
-                                if(err.message.includes('need to be inviter'))
-                                {
-                                    bot.sendMessage(chatGroupId, 'No puedo bannear a nadie ya que no soy admin :(')
-                                        .catch(tgErr => mongo.logError(tgErr, chatGroupId));     
-                                }
-                                else
-                                {
-                                    mongo.logError(err, chatGroupId);
-                                }
-                            });
-                            break;
-                        default:
-                            bot.sendMessage(chatId, "Sory" + userMention + " pero no sos admin.", { parse_mode: generic.markDownParseMode });
-                            break;
+                            return;
                     }
-                }).catch(e => mongo.logError(e, chatGroupId));     
-            });                                
-        });
+
+                    bot.getChatMember(chatId, userId).then(userMember => {                    
+            
+                        switch(userMember.status)
+                        {
+                            case generic.statusCreator:
+                            case generic.statusAdministrator:
+                                bot.kickChatMember(chatGroupId, replyId).then(() => {
+                                    bot.sendMessage(chatGroupId, userMention + ' ha baneado a ' + replyMention + '!', { parse_mode: generic.markDownParseMode })
+                                        .catch(err => mongo.logError(err, chatGroupId));;
+                                })
+                                .catch(err => {
+                                    if(err.message.includes('need to be inviter'))
+                                    {
+                                        bot.sendMessage(chatGroupId, 'No puedo bannear a nadie ya que no soy admin :(')
+                                            .catch(tgErr => mongo.logError(tgErr, chatGroupId));     
+                                    }
+                                    else
+                                    {
+                                        mongo.logError(err, chatGroupId);
+                                    }
+                                });
+                                break;
+                            default:
+                                bot.sendMessage(chatId, "Sory" + userMention + " pero no sos admin.", { parse_mode: generic.markDownParseMode });
+                                break;
+                        }
+                    }).catch(e => mongo.logError(e, chatGroupId));     
+                });                                
+            });
+        }).catch(error => reject(error));
     });
 };
-
-
 
 // exports.execute = (bot, msg) => {
 //     if(msg.reply_to_message == undefined)
