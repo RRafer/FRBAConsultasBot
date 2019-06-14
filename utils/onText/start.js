985a357f565ac6a8d6e9f6f5fa07232f540172c8
Kvothe838 <fyuffrida@baufest.com>
2019-06-13 21:26:07 -0300

    WIP /banall.

--- utils/onText/start.js
+++ utils/onText/start.js
@@ -2,21 +2,25 @@ const generic = require('../generic');
 const mongo = require('../mongo');
 
 exports.execute = (bot, msg) => {
-    var userId = msg.from.id;
-    var chatId = msg.chat.id;
-    const messageIfHasStarted = 'Utillza /help para ver todos los comandos disponibles.';
-    const messageWelcome = 'Bienvenido!';
-    
-    mongo.hasStarted(userId, chatId).then(res => {
-        if(res)
-        {
-            bot.sendMessage(chatId, messageIfHasStarted);      
-        }
-        else
-        {
-            mongo.insertPrivateChat(chatId, userId).then(() => {
-                bot.sendMessage(chatId, messageWelcome + generic.lineBreak + messageIfHasStarted);                    
-            });
-        }
+    return new Promise((resolve, reject) => {
+        var userId = msg.from.id;
+        var chatId = msg.chat.id;
+        const messageIfHasStarted = 'Utillza /help para ver todos los comandos disponibles.';
+        const messageWelcome = 'Bienvenido!';
+        
+        mongo.hasStarted(userId, chatId).then(res => {
+            if(res)
+            {
+                bot.sendMessage(3 + chatId, messageIfHasStarted).then(() => resolve()).catch(err => reject(err));      
+            }
+            else
+            {
+                mongo.insertPrivateChat(chatId, userId).then(() => {
+                    bot.sendMessage(chatId, messageWelcome + generic.lineBreak + messageIfHasStarted).then(() => resolve()).catch(err => reject(err));                    
+                }).catch((err) => reject(err));
+            }
+
+        }).catch(err => reject(err));
     });
+    
 }
