39ecfe4948e53f6a62e9c0fca52dc795d5857705
Kvothe838 <fyuffrida@baufest.com>
2019-06-13 21:26:07 -0300

    WIP /banall.

--- utils/mongo/errors.js
+++ utils/mongo/errors.js
@@ -7,18 +7,27 @@ const url = 'mongodb://localhost:27017';
 //type - string
 
 exports.logError = (error, chatId) => {
+    console.log("HUBO UN ERROR: " + error);
     mongo.connect(url, (err, client) => {
-        if(client == undefined || client == null) return;
-        
-        const db = client.db('telegram');
-        const col = db.collection('errors');
-  
-        if(err || !error) return;
-        
-        col.insertOne({'chatId': chatId, 'errorMsg': error.message, 'date': new Date().toDateString()}, (err, result) => {
-            client.close();
-
-            if(err) return;
-        });
-      });
+        try
+        {
+            const db = client.db('telegram');
+            const col = db.collection('errors');
+            
+            if(err || !error){
+                client.close();
+                return;
+            }
+            
+            col.insertOne({'chatId': chatId, 'errorName': error.name, 'lineNumber': error.lineNumber, 'errorMsg': error.message}, (err, result) => {
+                client.close();
+    
+                if(err) return;
+            }).catch(e => console.log(e));
+        }
+        catch(e)
+        {
+            console.log(e);
+        }        
+    });
 }
--- utils/onText/start.js
+++ utils/onText/start.js
@@ -3,27 +3,24 @@
 
 exports.execute = (bot, msg) => {
     return new Promise((resolve, reject) => {
+        var userId = msg.from.id;
         var chatId = msg.chat.id;
         const messageIfHasStarted = 'Utillza /help para ver todos los comandos disponibles.';
-        bot.sendMessage(chatId, messageIfHasStarted).then(() => resolve()).catch(error => reject(error));    
+        const messageWelcome = 'Bienvenido!';
+        
+        mongo.hasStarted(userId, chatId).then(res => {
+            if(res)
+            {
+                bot.sendMessage(3 + chatId, messageIfHasStarted).then(() => resolve()).catch(err => reject(err));      
+            }
+            else
+            {
+                mongo.insertPrivateChat(chatId, userId).then(() => {
+                    bot.sendMessage(chatId, messageWelcome + generic.lineBreak + messageIfHasStarted).then(() => resolve()).catch(err => reject(err));                    
+                }).catch((err) => reject(err));
+            }
+
+        }).catch(err => reject(err));
     });
+    
 }
-
-//#region Comentarios
-
-//var userId = msg.from.id;
-//const messageWelcome = 'Bienvenido!';
-// mongo.hasStarted(userId, chatId).then(res => {
-//     if(res)
-//     {
-//         bot.sendMessage(chatId, messageIfHasStarted);      
-//     }
-//     else
-//     {
-//         mongo.setStartedUser(userId, chatId).then(() => {
-//             bot.sendMessage(chatId, messageWelcome + generic.lineBreak + messageIfHasStarted);                    
-//         });
-//     }
-// });
-
-//#endregion
