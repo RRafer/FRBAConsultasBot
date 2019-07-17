6daf1f9d97f359f39d3c54c2510d395a7b7b5a3d
nico2236 <nico2236@gmail.com>
2019-07-17 17:34:26 -0300

    small fix for config features

--- utils/config.js
+++ utils/config.js
@@ -6,7 +6,13 @@ let config = {
 if (config.mongoEnabled) {
   config.features = loadConfigFromDB();
 } else {
-  config.features[-1001214086516] = {
+  config.features[-1001262375149] = {
+    enableDeleteSystemMessages: false,
+    enableLinks: false,
+    enableValidateUsers: true,
+  };
+  // defaults
+  config.features[0] = {
     enableDeleteSystemMessages: false,
     enableLinks: false,
     enableValidateUsers: true,
