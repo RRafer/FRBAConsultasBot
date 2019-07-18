5c68854324fc4f718ddd8b1aaf2aa117a2d2a77e
RRafer <federicoragazzo1999@hotmail.com>
2019-07-18 14:32:38 -0300

    cmd /google finalizado

--- bot.js
+++ bot.js
@@ -50,8 +50,8 @@ bot.on('message', (msg) => {
 	if ((config.features[msg.chat.id]
     && config.features[msg.chat.id].enableValidateUsers)
     || config.features[0].enableValidateUsers) {
-		adminUtils.validateUser(bot);
-	}
+    adminUtils.validateUser(bot);
+  }
 });
 
 // Envia links de grupos y otros
@@ -63,6 +63,12 @@ bot.onText(/^\/links/,
 			linksUtils.sendLinks(bot);
 		}
 	});
+
+// LMGTFY
+bot.onText(/^\/google (.*)/ , (msg, match) => {
+  bot.sendMessage(msg.chat.id, `https://lmgtfy.com/?q=${encodeURIComponent(match[1])}`, {reply_to_message_id: msg.message_id});
+});
+
 
 /*
 bot.onText(/^\/catedra/, (msg) => {
