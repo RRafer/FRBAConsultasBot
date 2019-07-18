a14f2589622a2a91dcb3ff0c6a123bd22917f95a
RRafer <federicoragazzo1999@hotmail.com>
2019-07-18 18:06:28 -0300

    /rotar mejorado

--- utils/config.js
+++ utils/config.js
@@ -10,14 +10,14 @@ if (config.mongoEnabled) {
 		enableDeleteSystemMessages: false,
 		enableLinks: false,
 		enableValidateUsers: true,
-		enableRotate: true,
+		enableRotate: false,
 	};
 	// defaults
 	config.features[0] = {
 		enableDeleteSystemMessages: false,
 		enableLinks: false,
 		enableValidateUsers: true,
-		enableRotate: true,
+		enableRotate: false,
 	};
 }
 module.exports = { config };
