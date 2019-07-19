8fd3e396946477efb9575375a6f2807ef859ba6f
nico2236 <nico2236@gmail.com>
2019-07-18 23:26:25 -0300

    WIP: refactor remindMe

--- utils/onText/remindme.js
+++ utils/onText/remindme.js
@@ -1,65 +1,58 @@
 const generic = require('../generic');
+const moment = require('moment');
 
 exports.execute = (bot, msg, match) => {
-	return new Promise((resolve, reject) => {
-		try
-		{
-			const chatId = msg.chat.id;
-			const userId = msg.from.id;    
-			const mention = generic.generateMention(msg);    
-			const replyMessageId = (msg.reply_to_message) ? msg.reply_to_message.message_id : null;
-			const Amount = match[1];
-			let typeOfUnit = match[2]; //days or whatever
             
-			const textToRemember = match[3];
+	// Esto es una negrada
+	if(!['days', 'hours', 'minutes', 'day','hour','minute'].includes(match[2]))
+		return;
 
-			const amount = parseInt(Amount);    
-			const results = generic.generateTimeInMiliseconds(typeOfUnit, amount);
-			const timeToSet = results[0];
-			const spanishTypeOfUnit = results[1];
-			var counter = 0;
+	const chatId = msg.chat.id;
+	const userId = msg.from.id;    
+	const mention = generic.generateMention(msg);    
+	const replyMessageId = (msg.reply_to_message) ? msg.reply_to_message.message_id : null;        
+	const textToRemember = match[3]; // extra text
+            
+	const timeToSet = moment().add(parseInt(match[1]), match[2]).unix();
+	// const spanishTypeOfUnit = results[1];
+            
+	//Que es esto? WHYYYYYYY
+	var counter = 0;
 
-			bot.sendMessage(chatId, `Te voy a recordar dentro de ${amount} ${spanishTypeOfUnit} ${mention}!`, { parse_mode: generic.markDownParseMode }).catch(e => reject(e));
-			setInterval(() => {
-				counter++;
+    // Esta parte... esta bien. PONELE ahora que borre variables, no.
+	bot.sendMessage(chatId, `Te voy a recordar dentro de ${amount} ${spanishTypeOfUnit} ${mention}!`, { parse_mode: 'Markdown' }).catch(e => reject(e));
+	// Consultar a la DB aca
+    // Hay que guardar todo en la DB y hacer un job que la consulte cada TANTO para ver si tiene que tirar algun timeout
+    // O sea, cambiar todo de aca para abajo. Lo dejo de referencia
+	setInterval(() => {
+		counter++;
 
-				if(counter == timeToSet)
-				{
-					bot.getChatMember(chatId, userId).then(member => {
-						switch(member.status)
-						{
-						case generic.statusKick:
-						case generic.statusLeft:
-							return;
-						}
-
-						if(replyMessageId != null)
-						{
-							
-							bot.sendMessage(chatId, `Te recuerdo ${mention}!`, {
-								reply_to_message_id: replyMessageId, 
-								parse_mode: generic.markDownParseMode
-							}).catch(() => {
-								reject();
-							});
-						}
-						else
-						{
-							bot.sendMessage(chatId, `Te recuerdo ${textToRemember || ''} ${mention} !`, {
-								parse_mode: generic.markDownParseMode
-							}).catch(() => {
-								reject();
-							});
-						}
-					}).catch(e => reject(e));
-				}
-			}, 1000);
-		}
-		catch(error)
+		if(counter == timeToSet)
 		{
-			reject(error);
+			bot.getChatMember(chatId, userId).then(member => {
+				if(member.status == 'kicked' || member.status == 'left')
+					return;
+
+
+				if(replyMessageId != null)
+				{
+							
+					bot.sendMessage(chatId, `Te recuerdo ${mention}!`, {
+						reply_to_message_id: replyMessageId, 
+						parse_mode: generic.markDownParseMode
+					}).catch(() => {
+						reject();
+					});
+				}
+				else
+				{
+					bot.sendMessage(chatId, `Te recuerdo ${textToRemember || ''} ${mention} !`, {
+						parse_mode: generic.markDownParseMode
+					}).catch(() => {
+						reject();
+					});
+				}
+			}).catch(e => reject(e));
 		}
-        
-	});
-    
+	}, 1000);
 };
