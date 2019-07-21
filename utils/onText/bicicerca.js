cc981310f8467cba7e6ffadc1692c70ee8bbb5b2
Muabid <bruno2698@gmail.com>
2019-07-21 14:42:19 -0300

    resolve bicimascercana

--- utils/onText/bicicerca.js
+++ utils/onText/bicicerca.js
@@ -1,47 +1,72 @@
 const request = require('request');
-const { clientId } = require('../token');
-const { clientSecret } = require('../token');
+const {clientId} = require('../token');
+const {clientSecret} = require('../token');
+
+
+function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+    var R = 6371; // Radius of the earth in km
+    var dLat = deg2rad(lat2 - lat1);  // deg2rad below
+    var dLon = deg2rad(lon2 - lon1);
+    var a =
+        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+        Math.sin(dLon / 2) * Math.sin(dLon / 2)
+    ;
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var d = R * c; // Distance in km
+    return d;
+}
+
+function deg2rad(deg) {
+    return deg * (Math.PI / 180)
+}
+
+
+function nearestStation(origin, station1, station2) {
+    var dist1 = getDistanceFromLatLonInKm(origin.latitude, origin.longitude, station1.lat, station1.lon)
+    var dist2 = getDistanceFromLatLonInKm(origin.latitude, origin.longitude, station2.lat, station2.lon)
+
+    return dist1 < dist2 ? station1 : station2
+}
+
+
+function stationsWithBikes(station) {
+    request({
+        url: `https://apitransporte.buenosaires.gob.ar/ecobici/gbfs/stationStatus?client_id=${clientId}&client_secret=${clientSecret}`,
+        json: true
+    }, (e, response, body) => {
+
+        if (!e && response.statusCode === 200) {
+
+            return body.data.stations.filter(s => s.num_bikes_available > 0)
+        }
+    });
+}
 
 exports.execute = (bot, msg) => {
-	if(msg.reply_to_message.location){
-        bot.deleteMessage(msg.chat.id, msg.reply_to_message.message_id);
-		request({
-			url: `https://apitransporte.buenosaires.gob.ar/ecobici/gbfs/stationInformation?client_id=${clientId}&client_secret=${clientSecret}`,
-			json: true
-		}, (e, response, body) => {
-			if(!e && response.statusCode === 200){
-                let lowdist, lat, lon;
-                
-				for(let i = 0; i < body.data.stations.length; i++){
-					let lat1 = msg.reply_to_message.location.latitude;
-					let lat2 = JSON.stringify(body.data.stations[i].lat);
-					let lon1 = msg.reply_to_message.location.longitude;
-					let lon2 = JSON.stringify(body.data.stations[i].lon);
-                    
-                    let R = 6371; // km (change this constant to get miles)
-					let dLat = (lat2-lat1) * Math.PI / 180;
-					let dLon = (lon2-lon1) * Math.PI / 180;
-					let a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-		                    Math.cos(lat1 * Math.PI / 180 ) * Math.cos(lat2 * Math.PI / 180 ) *
-		                    Math.sin(dLon/2) * Math.sin(dLon/2);
-					let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-                    let d = R * c;
-                    
-                    console.log(R);
-                    console.log(dLat);
-                    console.log(dLon);
-                    console.log(a);
-                    console.log(c);
-                    console.log(d);
-                    
-					if(d < lowdist){
-                        lowdist = d;
-						lat = lat2;
-                        lon = lon2;
-					}
-                }		
-			}
-		});
-	}
-    
+    if (msg.reply_to_message.location) {
+        //bot.deleteMessage(msg.chat.id, msg.reply_to_message.message_id);
+        request({
+            url: `https://apitransporte.buenosaires.gob.ar/ecobici/gbfs/stationInformation?client_id=${clientId}&client_secret=${clientSecret}`,
+            json: true
+        }, (e, response, body) => {
+
+            if (!e && response.statusCode === 200) {
+
+                let locationUsr = msg.reply_to_message.location
+                console.log(locationUsr)
+                let station = body.data.stations
+                    .reduce((station1, station2) => nearestStation(locationUsr, station1, station2))
+
+                bot.sendLocation(msg.chat.id, station.lat, station.lon).then(value => {
+
+                    //TODO: Deberiamos mandar la cantidad de bicis disponibles
+                })
+
+            }
+        });
+    }
+
 };
+
+
