baf0fc50cc0a59742bf72f00a7332c251eb14837
Julia Morelli <jmlanghe@gmail.com>
2019-09-26 19:42:43 -0300

    Improve controller design
    * Implement basic controllers idea :sleeping:
    
    * Working isEnabledFor + User validation

--- utils/config.js
+++ utils/config.js
@@ -1,23 +1,42 @@
-let config = {
-	mongoEnabled: false,
-	features: {},
+/**
+ * Returns an object with config for each chat
+ * @param {boolean} mongoEnabled 
+ * @returns {Object}
+ */
+const config = (mongoEnabled) => {
+	if (mongoEnabled) return loadConfigFromDB();
+	return {
+		'-1001262375149': {
+			enableDeleteSystemMessages: false,
+			enableLinks: false,
+			enableValidateUsers: true,
+			enableRotate: false,
+			enableGoogle: false,
+		},
+		// defaults
+		'0': {
+			enableDeleteSystemMessages: false,
+			enableLinks: false,
+			enableValidateUsers: true,
+			enableRotate: false,
+			enableGoogle: false,
+		},
+	};
 };
 
-if (config.mongoEnabled) {
-	config.features = loadConfigFromDB();
-} else {
-  config.features[-1001262375149] = {
-    enableDeleteSystemMessages: false,
-    enableLinks: false,
-    enableValidateUsers: true,
-    enableGoogle: false,
-  };
-  // defaults
-  config.features[0] = {
-    enableDeleteSystemMessages: false,
-    enableLinks: false,
-    enableValidateUsers: true,
-    enableGoogle: false,
-  };
-}
-module.exports = { config };
+// No quiero usar THIS pero la otra manera que se me ocurre es pasarle un parametro mas que sea la config.
+// Idk
+// ~Loki
+
+/**
+ * Check if feature is enabled for that chatId.
+ * @param {string} feature 
+ * @param {number} chatId 
+ * @returns {boolean} If that chat supports that function
+ */
+function isEnabledFor (feature, chatId) {return (this.features[chatId] && this.features[chatId][feature]) || this.features[0][feature]; };
+
+module.exports = {
+	features: config(false),
+	isEnabledFor: isEnabledFor,
+};
