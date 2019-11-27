e81bff009481c509fcbe9c31c25ecc51ed8ff500
nico2236 <nico2236@gmail.com>
2019-11-26 23:44:18 -0300

    banall (prototype)

--- controllers/banall.js
+++ controllers/banall.js
@@ -4,64 +4,46 @@
 
 let groupIDs = ['-1001262375149','-1001214086516'];
 
-exports.banall = (bot, users, msg) => {
+exports.banall = (bot, usersList, msg) => {
 
-	//ID to ban: msg.reply_to_message.from.id
-	if (msg.reply_to_message){
-		bot.getChatMember(msg.chat.id, msg.from.id).then(userMember => {                    
-			if(userMember.status == 'creator' || userMember.status == 'administrator'){
-				//foreach group =>
-				groupIDs.forEach((chatGroupId) => {
-					bot.kickChatMember(chatGroupId, msg.reply_to_message.from.id).then(() => {
-						console.log('Banneado');
-					})
-						.catch(err => {
-							console.log(err);
-						});
-				});
+	let idToBan;
+	//Check Credentials of invoking user
+	bot.getChatMember(msg.chat.id, msg.from.id).then(userMember => {                    
+		if(userMember.status == 'creator' || userMember.status == 'administrator'){
+			//Bans author of the replied message
+			if (msg.reply_to_message){
+				idToBan = msg.reply_to_message.from.id;
 			}
-		});
-	}
-	else{
-		if (msg.entities && msg.entities[0].user){
-			//{ offset: 5, length: 9, type: 'mention' } 0 [ { offset: 5, length: 9, type: 'mention' } ]
-			//algo
-			bot.getChatMember(msg.chat.id, msg.from.id).then(userMember => {                    
-				if(userMember.status == 'creator' || userMember.status == 'administrator'){
-					//foreach group =>
-					groupIDs.forEach((chatGroupId) => {
-						bot.kickChatMember(chatGroupId, msg.entities[0].user.id).then(() => {
-							console.log('Banneado');
-						})
-							.catch(err => {
-								console.log(err);
-							});
-					});
-				}
-			});
-		
-		}else{
-			if (msg.entities && !msg.entities[0].user){
-			//{ offset: 5, length: 9, type: 'mention' } 0 [ { offset: 5, length: 9, type: 'mention' } ]
-				//algo
-				bot.getChatMember(msg.chat.id, msg.from.id).then(userMember => {                    
-					if(userMember.status == 'creator' || userMember.status == 'administrator'){
-						//foreach group =>
-						groupIDs.forEach((chatGroupId) => {
-							console.log(`Baneando: ${msg.text.substr(msg.entities[0].offset,msg.entities[0].length)}`);
-							bot.kickChatMember(chatGroupId, users.get(msg.text.substr(msg.entities[0].offset,msg.entities[0].length))).then(() => {
-								console.log('Banneado');
-							})
-								.catch(err => {
-									console.log(err);
+			else{
+				//If user is mentioned with @username
+				if (msg.entities){
+					msg.entities.forEach((entity) => {
+						//If user is mentioned and it has @username
+						if (entity.type == 'text_mention'){
+							idToBan = entity.user.id;
+						}
+						else{
+							if (entity.type == 'mention'){
+								//If user is mentioned but has no @username
+								let nameToSearch = msg.text.substr(entity.offset+1,entity.length-1);								
+								usersList.forEach((v,k) => {
+									if (v == nameToSearch){
+										idToBan = k;
+									}
 								});
-						});
-					}
-    
-				});
+							}
+						}
+					});
+					
+				}
 			}
-			//Text mention now:
-    
+			groupIDs.forEach((chatGroupId) => {
+				bot.kickChatMember(chatGroupId, idToBan).then(() => {
+				}).catch(err => {
+					console.log(err);
+				});
+			});
 		}
-	}
+    
+	});
 };
