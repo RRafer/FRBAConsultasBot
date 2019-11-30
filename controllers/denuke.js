da774f5e064ba15f5fbad6d4e60832a19dd12051
nico2236 <28462447+loki2236@users.noreply.github.com>
2019-11-30 08:13:13 -0300

    Removing old dependencies

--- controllers/denuke.js
+++ controllers/denuke.js
@@ -1,12 +1,12 @@
 /* eslint-disable no-console */
-
+const { generateMention } = require('../utils/generic');
 //ids, temporal hasta que los carguemos en la DB
 
 let groupIDs = ['-1001262375149','-1001214086516', '-1001155863433', '-1001249368906', '-1001387811266', '-1001172707925','-1001386498425','-1001205439751','-1001337509181','-1001259839523','-1001289702550','-1001255281308','-1001171982049','-1001203933567','-1001313951685', '-1001157259051', '-1001290861768', '-1001378858456', '-1001288012396','-1001485242696', '-1001370286549', '-1001394632264', '-1001286595347', '-1001396035324', '-1001177806644'];
 
 exports.denuke = (bot, usersList, msg) => {
 
-	let idToBan;
+	let idToBan, mentionToBan;
 	//Check group
 	if (groupIDs.includes(msg.chat.id)){
 	//Check Credentials of invoking user
@@ -15,6 +15,7 @@ exports.denuke = (bot, usersList, msg) => {
 			//Bans author of the replied message
 				if (msg.reply_to_message){
 					idToBan = msg.reply_to_message.from.id;
+					mentionToBan = generateMention(msg.reply_to_message);
 				}
 				else{
 				//If user is mentioned with @username
@@ -23,6 +24,8 @@ exports.denuke = (bot, usersList, msg) => {
 						//If user is mentioned and it has @username
 							if (entity.type == 'text_mention'){
 								idToBan = entity.user.id;
+								let fromName = `${entity.user.first_name}${entity.user.last_name ? ` ${entity.user.last_name}` : ''}`;
+								mentionToBan = `[@${entity.user.username||fromName}](tg://user?id=${entity.user.id})`; 
 							}
 							else{
 								if (entity.type == 'mention'){
@@ -31,6 +34,7 @@ exports.denuke = (bot, usersList, msg) => {
 									usersList.forEach((v,k) => {
 										if (v == nameToSearch){
 											idToBan = k;
+											mentionToBan = `[@${v}](tg://user?id=${idToBan})`;
 										}
 									});
 								}
@@ -46,6 +50,7 @@ exports.denuke = (bot, usersList, msg) => {
 								bot.getChatMember(msg.chat.id, idToBan).then((userToBan) => {
 									if(userToBan.status != 'creator' || userToBan.status != 'administrator'){
 										bot.unbanChatMember(chatGroupId, idToBan).then(() => {
+											bot.sendMessage(chatGroupId, `${generateMention(msg)} ha desbaneado a ${mentionToBan} !`, { parse_mode: 'Markdown' });
 										}).catch(err => {
 											//Catch 'cannot ban user' errors
 											console.log(err);
