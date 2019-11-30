3c5543ba2cf64cdc68b06b7104e01fb653e4fb56
nico2236 <28462447+loki2236@users.noreply.github.com>
2019-11-30 10:07:55 -0300

    Nuke/denuke working

--- controllers/nuke.js
+++ controllers/nuke.js
@@ -1,12 +1,14 @@
+// @ts-check
 /* eslint-disable no-console */
 const { generateMention } = require('../utils/generic');
 //ids, temporal hasta que los carguemos en la DB
 
-let groupIDs = ['-1001262375149','-1001214086516', '-1001155863433', '-1001249368906', '-1001387811266', '-1001172707925','-1001386498425','-1001205439751','-1001337509181','-1001259839523','-1001289702550','-1001255281308','-1001171982049','-1001203933567','-1001313951685', '-1001157259051', '-1001290861768', '-1001378858456', '-1001288012396','-1001485242696', '-1001370286549', '-1001394632264', '-1001286595347', '-1001396035324', '-1001177806644'];
+let groupIDs = ['-1001214086516','-1001262375149','-1001214086516', '-1001155863433', '-1001249368906', '-1001387811266', '-1001172707925','-1001386498425','-1001205439751','-1001337509181','-1001259839523','-1001289702550','-1001255281308','-1001171982049','-1001203933567','-1001313951685', '-1001157259051', '-1001290861768', '-1001378858456', '-1001288012396','-1001485242696', '-1001370286549', '-1001394632264', '-1001286595347', '-1001396035324', '-1001177806644'];
 
 exports.nuke = (bot, usersList, msg) => {
 
 	let idToBan, mentionToBan;
+	console.log('Nuking....');
 	//Check group
 	if (groupIDs.includes(msg.chat.id)){
 	//Check Credentials of invoking user
@@ -47,11 +49,11 @@ exports.nuke = (bot, usersList, msg) => {
 					groupIDs.forEach((chatGroupId) => {
 						bot.getChatMember(msg.chat.id, msg.from.id).then(userMember => {                    
 							if(userMember.status == 'creator' || userMember.status == 'administrator'){
-
 								bot.getChatMember(msg.chat.id, idToBan).then((userToBan) => {
 									if(userToBan.status != 'creator' && userToBan.status != 'administrator'){
 										bot.kickChatMember(chatGroupId, idToBan).then(() => {
-											bot.sendMessage(chatGroupId, `${generateMention(msg)} ha kickeado a ${mentionToBan} !`, { parse_mode: 'Markdown' });
+											if (chatGroupId == msg.chat.id)
+												bot.sendMessage(chatGroupId, `${generateMention(msg)} ha kickeado a ${mentionToBan} !`, { parse_mode: 'Markdown' });
 										}).catch(err => {
 											//Catch 'cannot ban user' errors
 											console.log(err);
