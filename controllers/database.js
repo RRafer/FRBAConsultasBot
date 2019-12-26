9da938a4eea5152e44cefcefdb5af6349addfc51
Loki2236 <28462447+loki2236@users.noreply.github.com>
2019-12-26 13:57:22 -0300

    MongoDB Singleton implementation & Example

--- controllers/database.js
+++ controllers/database.js
@@ -1,52 +1,22 @@
-require('dotenv').config();
 const MongoClient = require('mongodb').MongoClient;
-const logger = require('./logger');
 
 // Mongo Config
-const url = process.env.DATABASE_URL || 'mongodb://localhost:27017';
-const dbName = process.env.DATABASE_NAME || 'telegrambot';
-let _db;
-let connecting = false;
+const url = 'mongodb://localhost:27017';
+const dbName = 'telegrambot';
+let _db = undefined;
 let Database = function(){};
 
 Database.initDb = async function (){
-	let con = await MongoClient.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
-	_db = con.db(dbName);
+	_db = await (await MongoClient.connect(url)).db(dbName);
 };
 
-Database.getDb = async function (){
-	if (_db == undefined && connecting == false){
-		logger.info('DB Not started... connecting');
-		connecting = true;
-		await Database.initDb();
-		logger.info('Connected');
+Database.getDb = function (){
+	if (_db == undefined){
+		console.log('DB Not started... connecting');
+		Database.initDb();
+		console.log('Connected');
 	}
 	return _db; 
 };
-
-
-
-//#region Comentarios
-
-// exports.setStartedUser = (userId, chatId) => users.setStartedUser(userId).catch(err => errors.logError(err, chatId));
-// exports.getPrivateChatId = (userId, chatId) => users.getPrivateChatId(userId).catch(err => errors.logError(err, chatId));
-// exports.hasStarted = (userId, chatId) => users.hasStarted(userId).catch(err => errors.logError(err, chatId));
-
-
-// Mongo Singleton Use Example
-// Placeholder name, change for each collection.
-/* const collectionName = 'mensajes';
-
-async function getdocs() {
-	let collection;
-	(await db.getDb()).then((db)=> {collection = db.collection(collectionName);});
-	collection.find({}).toArray(function(err, docs) {
-		if (!err)
-			console.log(`Se han cargado ${docs.length} documentos`);
-	});
-}
-*/
-//#endregion
-
 
 module.exports = Database;
