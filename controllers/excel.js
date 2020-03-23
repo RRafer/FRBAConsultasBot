a08ab34593c3356af3545a450568250e54944f04
kvothe838 <facundo@omniasalud.com>
2020-03-22 23:46:30 -0300

    Attempt to fix callback hell.

--- controllers/excel.js
+++ controllers/excel.js
@@ -7,15 +7,15 @@ exports.excel = async (bot, msg) => {
 
 	// Check group
 	if (!groupIDs.includes(msg.chat.id)) return;
-	
-	bot.sendMessage(msg.chat.id, `¿Te fijaste en el <a href=\'https://docs.google.com/spreadsheets/d/19XPRP8zsVut-W1HihBxSZ_mZHlyHMBy-WIKNqurdNs8/edit?pref=2&pli=1#gid=1173209158\'>Excel de profesores</a>?`,
+
+	await bot.sendMessage(msg.chat.id, `¿Te fijaste en el <a href=\'https://docs.google.com/spreadsheets/d/19XPRP8zsVut-W1HihBxSZ_mZHlyHMBy-WIKNqurdNs8/edit?pref=2&pli=1#gid=1173209158\'>Excel de profesores</a>?`,
 		{ 
 			parse_mode: 'HTML', 
 			reply_to_message_id: msg.reply_to_message ? msg.reply_to_message.message_id : '', 
 			disable_web_page_preview: true 
-		}).then(() => {
-			setTimeout(() => 
-				bot.deleteMessage(msg.chat.id, msg.message_id).catch(err => logger.error(`Error deleting excel message: ${err}`)),
-				120000)
 		}).catch(err => logger.error(`Error sending excel message: ${err}`));	
+	
+	setTimeout(() => 
+		bot.deleteMessage(msg.chat.id, msg.message_id).catch(err => logger.error(`Error deleting excel message: ${err}`)),
+		120000);
 };
