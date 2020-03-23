9732b54e9bde6ccca14513499a3005e4fc928afa
Kvothe838 <35804584+Kvothe838@users.noreply.github.com>
2020-03-23 10:13:15 -0300

    Add excel command and generic methods
    *Added excel command, which sends a message with the professor's Excel link as a reply to the original message or as a simple message. Also, the message with the command is deleted after two minutes.
    *Added generic methods: isAdmin and getUserStatus.

--- bot.js
+++ bot.js
@@ -8,6 +8,7 @@ const linksController = require('./controllers/links');
 const adminControllers = require('./controllers/admin');
 const nuke = require('./controllers/nuke');
 const denuke = require('./controllers/denuke');
+const excel = require('./controllers/excel');
 const config = require('./utils/config');
 const { token } = require('./utils/token');
 const onText = require('./utils/onText/onText');
@@ -38,7 +39,7 @@ bot.on('message', (msg) => {
      (msg.new_chat_members !== undefined || msg.left_chat_member !== undefined)){
 		// I have to work this over
 		bot.deleteMessage(msg.chat.id, String(msg.message_id)).catch(e =>{
-			logger.error(`Error Eliminando mensaje: ${e}`);
+			logger.error(`Error deleting message: ${e}`);
 		});
 	}
 
@@ -51,7 +52,7 @@ bot.on('message', (msg) => {
 	// I've never seen this user before
 	if(!savedUsers.has(msg.from.id) || savedUsers.get(msg.from.id) !== msg.from.username){
 		savedUsers.set(msg.from.id, msg.from.username);
-		logger.info(`Agregado/Actualizado ${msg.from.username} a la lista de usuarios`);
+		logger.info(`Added/updated ${msg.from.username} to the list of users`);
      
 	} 
 });
@@ -86,7 +87,72 @@ bot.onText(/^\/google (.+)/ , (msg, match) => {
 	if (config.isEnabledFor('enableGoogle', msg.chat.id)) 
 		bot.sendMessage(msg.chat.id, `https://lmgtfy.com/?q=${encodeURIComponent(match[1])}`, {reply_to_message_id: msg.message_id});
 });
-//TODO: Revisar todo esto para la parte de links
+
+bot.onText(/^\/excel/, msg => {
+  if(config.isEnabledFor('enableExcel', msg.chat.id))
+    excel.excel(bot, msg);
+});
+
+
+/*
+bot.onText(/^\/catedra/, (msg) => {
+  const chatPos = idChatPhoto.indexOf(msg.chat.id);
+  if (chatPos === -1) {
+    bot.sendPhoto(msg.chat.id, 'AgADAQADbKgxG103QUQeGv8r315mbltxDDAABECS2Sc_YjetEiQFAAEC').then((auxid) => {
+      setTimeout(() => {
+        borrarchat = idChatPhoto.indexOf(auxid.chat.id);
+        idChatPhoto.splice(borrarchat, 1);
+        idPhoto.splice(borrarchat, 1);
+      }, 60000);
+      idChatPhoto.push(auxid.chat.id);
+      idPhoto.push(auxid.message_id);
+    });
+  } else {
+    bot.sendMessage(idChatPhoto[chatPos], 'Has solicitado el comando muy pronto. Aqui tienes la ultima vez que el comando ha sido usado', { reply_to_message_id: idPhoto[chatPos] });
+  }
+});
+
+bot.onText(/^\/sticker/, (msg) => {
+  if (msg.chat.id === -1001214086516) {
+    const posChat = stickerChat.indexOf(msg.from.id);
+    const stickerId = msg.from.id;
+    if (posChat === -1) {
+      if (Math.random() >= 0.5) {
+        bot.sendMessage(msg.chat.id, 'Cagaste bro, te re cagamo\' lo\' sticker');
+        bot.restrictChatMember(msg.chat.id, stickerId, {
+          can_send_message: true,
+          can_send_media_messages: false,
+          can_send_other_messages: false,
+          can_add_web_page_previews: false,
+        }).then(() => {
+          setTimeout(() => {
+            stickerChat.splice(posChat, 1);
+          }, 432000000);
+          stickerChat.push(stickerId);
+        });
+      } else {
+        bot.sendMessage(msg.chat.id, 'Nos cagaste.\nTe devolvimos los permisos loro');
+        bot.promoteChatMember(msg.chat.id, stickerId, {
+          can_send_message: true,
+          can_send_media_messages: true,
+          can_send_other_messages: true,
+          can_add_web_page_previews: true,
+        });
+      }
+    } else {
+      bot.sendMessage(msg.chat.id, 'No puedes utilizar este comando hasta dentro de 12hs despues de haber utilizado el comando');
+    }
+  } else {
+    bot.deleteMessage(msg.chat.id, msg.message_id);
+    const deleteMsg = bot.sendMessage(msg.chat.id, 'No puede realizar la acción en este grupo\n\nEste mensaje se borrara en unos instantes').then((deleteMsg) => {
+      setTimeout(() => {
+        bot.deleteMessage(deleteMsg.chat.id, deleteMsg.message_id);
+      }, 10000);
+    });
+  }
+});
+*/
+
 bot.on('callback_query', (json) => {
 	const CBObject = JSON.parse(json.data);
 	if (CBObject.action === 'v') {
--- utils/generic.js
+++ utils/generic.js
@@ -1,5 +1,17 @@
+const logger = require('../controllers/logger');
+
 exports.generateMention = (msg) => {
 	const {username, first_name, last_name, id} = msg.from;
 	const fromName = `${first_name}${last_name ? ` ${last_name}` : ''}`;
 	return `[@${username||fromName}](tg://user?id=${id})`;
 };
+
+exports.getUserStatus = async (bot, msg) => {
+	logger.info('Getting user credentials');
+	const userMember = await bot.getChatMember(msg.chat.id, msg.from.id)
+			.catch(err => logger.error(`Cannot get user: ${err}`));
+
+	return userMember.status;
+};
+
+exports.isAdmin = (bot, msg) => ['creator', 'administrator'].includes(getUserStatus(bot, msg));
